8ed80d1f75c28d08f60719edd88552d976f88341
apivonka <alpivonka@gmail.com>
2015-05-06 15:34:36 -0400

    Update plot1.R
    adding comments

--- plot1.R
+++ plot1.R
@@ -1,10 +1,14 @@
 plot1<-function(){  
+  #Source the getData.R file which loads the data from a ./data directory and makes all changes required to the data before 
+  # it is needed by the plotting. Please review getData.R for the acquisition of data.
   source(file = "getData.R")
+  #Call the get data.
   data<-getData()
   
   png(filename = "plot1.png", width = 480, height = 480,units = "px", bg = "transparent")
-  
-  hist(x = data$Global_active_power, xlab = "Global Active Power (kilowatts)",col = "red",  main = "Global Active Power (Al Pivonka)", 
+  # create a histgram. Please not my name is within the plot.
+  hist(x = data$Global_active_power, xlab = "Global Active Power (kilowatts)",col = "red",  
+      main = "Global Active Power (Al Pivonka)", 
        breaks = 12, ylim = c(0, 1200))
   
   dev.off()
