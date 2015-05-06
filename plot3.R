b995f24e28c2ee94f398fc3c3740abcaac6c6128
apivonka <alpivonka@gmail.com>
2015-05-06 15:36:07 -0400

    Updating with comments

--- plot3.R
+++ plot3.R
@@ -1,4 +1,6 @@
 plot3<-function(){
+  #Source the getData.R file which loads the data from a ./data directory and makes all changes required to the data before 
+  # it is needed by the plotting. Please review getData.R for the acquisition of data.
   source(file = "getData.R")
   mydata<-getData()
   png("plot3.png",width=480,height=480,units="px",bg="transparent")
@@ -12,8 +14,8 @@ plot3<-function(){
   
   legend_text<-c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
   legend("topright",legend_text , cex=0.8, col=plot_colors,lty=1, lwd=2, bty="n");
+  #Adding my name to the plot
   title(main="Al Pivonka")
   dev.off()
-  print("done plot3")
   
 }
